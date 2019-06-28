83b27f5bcaa4eedacc8aca048547b7405135c4f4
jordanbacon <jmbacon92@gmail.com>
2019-06-27 17:50:14 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
+require 'pry'
+
 # Add your code here
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+    @name = name
+    @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        puts @@all.map{ |dog| dog.name }
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def name
+        @name
+    end
+
+end
--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -1,6 +1,5 @@
 require "spec_helper"
 
-
 describe "Dog" do
 
   let!(:pluto) { Dog.new("Pluto") }
